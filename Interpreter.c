7491a160925600f143ea511fd06d567db02ce32f
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2022-09-09 17:15:12 +0700

    Create Interpreter.c

---
+++ Interpreter.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int codeLenght;
+    puts("=======WELCOME TO MASSAGE ENCRIPTOR v.1.0.0=======");
+    puts("---------- by Muhammad Annys (L0122102) ----------");
+    puts("Space is counted as 1");
+    printf("Character amount in the text is "); scanf("%d\n", &codeLenght);
+    
+    int code[codeLenght];
+    scanf("%d", code);
+}
