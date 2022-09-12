0bdd76542935a909fa909bd682c9f6a941683295
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2022-09-12 13:16:44 +0700

    Update Max.c

--- Max.c
+++ Max.c
@@ -4,7 +4,11 @@ int Max(int, int, int);
 
 int main()
 {
-    printf("%d", Max(1, 2, 3));
+    int i, j, k;
+    printf("Angka 1 : "); scanf("%d", &i);
+    printf("Angka 2 : "); scanf("%d", &j);
+    printf("Angka 3 : "); scanf("%d", &k);
+    printf("%d", Max(i, j, k));
 
     return 0;
 }
@@ -13,13 +17,15 @@ int Max(int i, int j, int k){
     if(i > j){
         if(i > k){
             return i;
-        }else{
+        }
+        else{
             return k;
         }
     }else{
         if(j > k){
             return j;
-        }else{
+        }
+        else{
             return k;
         }
     }
