bd4fc0ed13cc7f56cd1d9aacaeaeb07a7af3184f
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2022-09-12 15:44:50 +0700

    Update Tugas KonPro3.c

--- Tugas KonPro3.c
+++ Tugas KonPro3.c
@@ -31,7 +31,7 @@ int main()
             puts("  4. [/] Pembagian");
             puts("  5. [\%] sisa");
             puts("");
-            printf("Panjang angka dibelakang koma : ");
+            printf("Panjang angka dibelakang koma (1 - 6) : ");
             scanf("%d", &floatLenght);
             puts("");
             scanf("%f %s %f", &num1, &opt, &num2);
