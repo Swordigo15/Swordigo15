ff9d44698b770a8d5c3de5220746e6987a9c1013
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2022-09-12 20:56:59 -0700

    Update Tugas KonPro4.c

--- Tugas KonPro4.c
+++ Tugas KonPro4.c
@@ -1,77 +1,104 @@
 #include <stdio.h>
 #include <math.h>
 
-int Luas(int, int, int);
+float Luas(int, float, float);
+float Keliling(int, float, float, float);
 
-int main()
+void main()
 {
-    int i, j;
-    int luas;
-    int keliling;
+    float x, y, z;
+    float l;
+    float k;
     
     int pilihanBangun;
-    scanf("%d", &pilihan);
+    puts("Jenis Bangun : ");
+    puts(" 1. Persegi");
+    puts(" 2. Persegi Panjang");
+    puts(" 3. Segitiga");
+    puts(" 4. Lingkaran");
+    printf("Pilihan anda : "); scanf("%d", &pilihanBangun);
+    
+    puts("======================================");
     
     int jenis;
-    scanf("%d", &jenis);
+    puts("Pilih operasi : ");
+    puts(" 1. Luas");
+    puts(" 2. Keliling");
+    printf("Pilihan anda : "); scanf("%d", &jenis);
     
+    puts("");
     switch(jenis){
         case 1:
-        break;
+            l = Luas(pilihanBangun, x, y);
+            printf("Luas     : %f\n", l);
+            break;
         case 2:
-        break;
+            k = Keliling(pilihanBangun, x, y, z);
+            printf("Keliling : %f", k);
+            break;
+        default:
+            puts("Operasi Tidak tersedia.");
+            break;
     }
-    switch(pilihanBangun){
+}
+
+float Luas(int j, float panjang, float lebar){
+    switch(j){
         case 1 :
             printf("Panjang sisi persegi : ");
-            scanf("%d", &i);
-            luas = Luas(pilihan, i, 0);
+            scanf("%f", &panjang);
+            return pow(panjang, 2);
             break;
         case 2 :
             printf("Panjang persegi panjang : ");
-            scanf("%d", &i);
-            printf("Lebar persegi panjang : ");
-            scanf("%d", &j);
-            luas = Luas(pilihan, i, j);
-            break;
-        case 3 :
-            printf("Alas sisi Segitiga : ");
-            scanf("%d", &i);
-            printf("Tinggi sisi Segitiga : ");
-            scanf("%d", &j);
-            luas = Luas(pilihan, i, j);
-            break;
-    }
-    
-    printf("luas     : %d\n", luas);
-    printf("keliling : %d", keliling);
-}
-
-int Luas(int j, int panjang, int lebar){
-    switch(j){
-        case 1:
-            return pow(panjang, 2);
-            break;
-        case 2:
+            scanf("%f", &panjang);
+            printf("Lebar persegi panjang   : ");
+            scanf("%f", &lebar);
             return panjang * lebar;
             break;
-        case 3:
+        case 3 :
+            printf("Alas sisi Segitiga   : ");
+            scanf("%f", &panjang);
+            printf("Tinggi sisi Segitiga : ");
+            scanf("%f", &lebar);
             return (panjang * lebar) / 2;
+            break;
+        case 4 :
+            printf("Panjang jari-jari lingkaran : ");
+            scanf("%f", &panjang);
+            return 3.14 * pow(panjang, 2);
             break;
     }
     return 0;
 }
 
-int keliling(int j, int panjang, int lebar, int sisi3){
+float Keliling(int j, float panjang, float lebar, float sisi3){
     switch(j){
-        case 1:
+        case 1 :
+            printf("Panjang sisi persegi : ");
+            scanf("%f", &panjang);
             return 4 * panjang;
             break;
-        case 2:
-            return 2 * (panjang + lebar);
+        case 2 :
+            printf("Panjang persegi panjang : ");
+            scanf("%f", &panjang);
+            printf("Lebar persegi panjang : ");
+            scanf("%f", &lebar);
+            return (panjang + lebar) * 2;
             break;
-        case 3:
+        case 3 :
+            printf("Sisi 1 sisi Segitiga : ");
+            scanf("%f", &panjang);
+            printf("Sisi 2 sisi Segitiga : ");
+            scanf("%f", &lebar);
+            printf("Sisi 3 sisi Segitiga : ");
+            scanf("%f", &sisi3);
             return panjang + lebar + sisi3;
+            break;
+        case 4 :
+            printf("Panjang diameter lingkaran : ");
+            scanf("%f", &panjang);
+            return 3.14 * panjang;
             break;
     }
     return 0;
