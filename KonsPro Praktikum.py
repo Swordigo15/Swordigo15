ae15359ef916810ef659c4f4645d1b1cc239375e
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2022-09-20 10:51:36 +0700

    Update KonsPro Praktikum.py

--- KonsPro Praktikum.py
+++ KonsPro Praktikum.py
@@ -10,10 +10,10 @@ for a in range(5):
 print()
 
 for x in range(n):                          #Horizontal
-    if x == 0 or x == n:
+    if x == 0 or x == n - 1:
         for y in range(n):
             print("*", end = " ")
-    elif x > 0 and x == (n + 1) / 2:
+    '''elif x > 0 and x == (n + 1) / 2:
         for y in range(n):
             if y == (n + 1) / 2:
                 print(",", end = " ")
@@ -21,8 +21,9 @@ for x in range(n):                          #Horizontal
                 print("*", end = " ")
     elif x == (n + 1) / 2:
         for y in range(n):
-            if y == 0 or y == n:
+            if y == 0 or y == n - 1:
                 print("*", end = " ")
             else:
                 print(",", end = " ")
+    '''
     print()
