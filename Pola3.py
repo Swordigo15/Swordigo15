5258fbf72ba6620be671ae45b91a24788393dc75
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2022-09-20 21:19:42 +0700

    Add files via upload

---
+++ Pola3.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+if n % 2 == 0:
+    n -= 1
+
+tengah = (n - 1) / 2
+
+for x in range(n): 
+    o = abs(x - tengah)
+    for y in range(n):
+        if y >= o and y <= (2 * tengah) - o:
+            print("*", end = " ")
+        else:
+            print(" ", end = " ")
+    print()
