fe209708feba3148d62b6d1cff951af6ab332e00
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2022-10-26 20:17:22 +0700

    Update Tugas Konspro7.c

--- Konsep Pemrograman/Tugas Konspro7.c
+++ Konsep Pemrograman/Tugas Konspro7.c
@@ -1,50 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef struct Nilai{
-    int UTS;
-    int UAS;
-    int NilaiAkhir;
-}nilai;
-
-typedef struct Identitas{
-    char nama[15];
+typedef struct Student{
+    char name[10];
     char NIM[8];
-    char kelas;
-    int umur;
-    nilai nilaiPtr;
-}identitas;
+    unsigned int age;
+}student;
 
-
-void SetIdentitas(identitas*, char[], char[], char, int, int, int, int);
+union Grade{
+    unsigned int exam[3];
+    float *avg;
+};
 
 int main()
 {
-    identitas id;
-    char n[15], nim[8], k;
-    int u, uts, uas, akhir;
+    student s;
+    union Grade g;
+    float total;
     
-    printf("Masukkan nama "); scanf("%s", n);
-    printf("Masukkan NIM "); scanf("%s", nim);
-    printf("Masukkan kelas "); scanf("%s", &k);
-    printf("Masukkan umur "); scanf("%u", &u);
-    printf("Masukkan nilai UTS "); scanf("%u", &uts);
-    printf("Masukkan nilai UAS "); scanf("%u", &uas);
-    printf("Masukkan nilai Akhir "); scanf("%u", &akhir);
-    SetIdentitas(&id, n, nim, k, u, uts, uas, akhir);
+    scanf("%s", s.name);
+    scanf("%s", s.NIM);
+    scanf("%u", &s.age);
+    scanf("%u", &g.exam[0]);
+    scanf("%u", &g.exam[1]);
+    scanf("%u", &g.exam[2]);
     
-    printf("Nama n %s\n", n);
-    printf("Nama \t\t: %s\nNIM \t\t: %s\nKelas \t\t: %c\nUmur \t\t: %d\nNilai UTS \t: %d\nNilai UAS \t: %d\nNilai Akhir \t: %d\n", 
-    id.nama, id.NIM, id.kelas, id.umur, id.nilaiPtr.UTS, id.nilaiPtr.UAS, id.nilaiPtr.NilaiAkhir);
+    total = (g.exam[0] + g.exam[0] + g.exam[0]) / 3;
+    g.avg = &total;
+    printf("\n%s%s\n%s%s\n%s%u%s\n%s%g",
+    "Your name is ", s.name,
+    "Your NIM is ", s.NIM,
+    "And you are ", s.age, " years old.",
+    "Your average is ", *g.avg);
+    
 }
 
-void SetIdentitas(identitas *i, char n[], char nim[], char k, int u, int uts, int uas, int akhir){
-    strcpy(i->nama, n);
-    strcpy(i->NIM, nim);
-    i->umur = u;
-    i->kelas = k;
-    i->nilaiPtr.UTS = uts;
-    i->nilaiPtr.UAS = uas;
-    i->nilaiPtr.NilaiAkhir = akhir;
+void DisplayBits(unsigned int value){
+    unsigned int displayMask = 1 << 31;
+    
+    printf("%10u = ", value);
 }
