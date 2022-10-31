29951b733e0dfcb2df32ed99bff0ced6b0b0049b
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2022-10-31 08:31:22 +0700

    Update Tugas KonsPro7.c

--- Konsep Pemrograman/Tugas KonsPro7.c
+++ Konsep Pemrograman/Tugas KonsPro7.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 
 typedef struct Student{
+    unsigned int number;
     char name[20];
     char NIM[9];
     unsigned int age;
-    union Nilai{
-        unsigned int mid;
-        unsigned int fin;
-        unsigned int avg;
-    } nilai;
+    union Grade{
+        unsigned int sisDig;
+        unsigned int konPro;
+        unsigned int kalku;
+        unsigned int fisika;
+        unsigned int statis;
+    } grade;
 }student;
 
+enum Course { 
+    SisDig = 1, KonPro,
+    Kalkulus, Fisika,
+    Statistiik
+};
+
 int ChooseMenu();
+void Save(FILE*);
+void InputData(FILE*);
+void CreateData(FILE*);
 
 int main()
 {
@@ -26,6 +38,12 @@ int main()
         while ((choice = ChooseMenu()) != 5) {
             switch(choice){
                 case 1:
+                    InputData(datPtr);
+                    Save(datPtr);
+                break;
+                case 2:
+                    CreateData(datPtr);
+                    Save(datPtr);
                 break;
                 default:
                     puts("Unrecognizable input");
@@ -43,21 +61,79 @@ void Save(FILE *readPtr){
     }
     else {
         rewind(readPtr);
-        fprintf(writePtr, "%-6s%-21s%-10s%-6s%-6s%-6s",
-                "No.", "Name", "NIM", "Mid", "Final", "Average");
+        fprintf(writePtr, "%-6s%-21s%-10s%-6s%-6s",
+                "No.", "Name", "NIM", "Age", "Grade");
                 
         while(!feof){
-            student sData = {"", "", 0, { 0 }};
-            
+            student sData = {0, "", "", 0, { 0 }};
+            size_t result = fread(&sData, sizeof(student), 1, readPtr);
+
+            if (result != 0 && sData.number != 0) {
+                fprintf(writePtr, "%-6d%-21s%-10s%-6d%-6d", 
+                        sData.number, sData.name, sData.NIM, sData.age, 
+                        sData.grade.sisDig + sData.grade.konPro + sData.grade.kalku
+                        + sData.grade.fisika + sData.grade.statis);
+            }
         }
+        fclose(writePtr);
+    }
+    puts("\t\tFile saved");
+}
+
+void InputData(FILE *fPtr){
+    int num = 0;
+    printf("\t\tEnter your last three number of NIM: "); scanf("%d", &num);
+    
+    fseek(fPtr, (num - 1) * sizeof(student), SEEK_SET);
+    
+    student sData = {0, "", "", 0, { 0 }};
+    fread(&sData, sizeof(student), 1, fPtr);
+    
+    if(sData.number == 0){
+        printf("\t\tNIM L0122%.3d has no information.\n", num);
+    }else{
+        printf("\t\t%-6d%-21s%-10s%-6d\n", sData.number, sData.name, sData.NIM, sData.age);
+        
+        fseek(fPtr, (num - 1) * sizeof(student), SEEK_SET);
+
+        fwrite(&sData, sizeof(student), 1, fPtr);
     }
 }
 
+void CreateData(FILE *fPtr){
+    int num = 0;
+    printf("\t\tEnter your last three number of NIM: "); scanf("%d", &num);
+    
+    fseek(fPtr, (num - 1) *  sizeof(student), SEEK_SET);
+    
+    student sData = {0, "", "", 0, { 0 }};
+    fread(&sData, sizeof(student), 1, fPtr);
+    
+    if(sData.number != 0){
+        printf("\t\tNIM L0122%.3d already has information.\n", num);
+    }else{
+        printf("\t\tEnter your name\t"); scanf("%19s", sData.name);
+        printf("\t\tEnter your age\t"); scanf("%5d", &sData.age);
+        
+        sData.number = num;
+        sprintf(sData.NIM, "L0122%.3d", num);
+        
+        fseek(fPtr, (sData.number - 1) * sizeof(student), SEEK_SET);
+
+        fwrite(&sData, sizeof(student), 1, fPtr);
+    }
+    
+}
+
 int ChooseMenu(){
-    printf("\t\t%s\n\t\t%s\n", 
+    printf("\t\t%s\n\t\t%s\n\t\t%s\n\t\t%s\n\t\t%s", 
             "----------MENU----------",
-            "1. Insert Nilai");
+            "1. Insert Grade",
+            "2. Create New",
+            "5. End Program",
+            "Your choice ");
     
     int c;
     scanf("%d", &c);
+    return c;
 }
