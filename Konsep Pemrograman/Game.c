807197851ce747bd47d0645c327de66a16214282
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2022-11-07 16:30:42 +0700

    Update Game.c

--- Konsep Pemrograman/Game.c
+++ Konsep Pemrograman/Game.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-enum EquipmentType { Armor = 1, Shield, Weapon };
+enum EquipmentType { Weapon, Shield, Armor };
 
 typedef struct{
     char name[12];
     int atk;
     int def;
-    enum EquipmentType *eqType;
+    enum EquipmentType eqType;
 }Equipment;
 
 typedef struct{
@@ -32,17 +32,18 @@ int main () {
     }else{
         fread(&player, sizeof(player), 1, PlayerSaveFile);
         
-        if(!(strcmp(player.name, "") == 0)){
+        printf("%d", strcmp(player.name, " ")==0);
+        if(strcmp(player.name, " ")==0){
             Start(&player);
-            printf("%s\n|Name\t: %-15s|\n|HP\t: %-15d|\n|ATK\t: %-15d|\n|DEF\t: %-15d|\n%s", 
+            printf("%s\n|Name\t: %-15s|\n|HP\t: %-15d|\n|ATK\t: %-15d|\n|DEF\t: %-15d|\n|EQ \t: %-15s|\n|\t- %-15s|\n|\t- %-15s|\n|\t- %-15s|\n%s", 
                 "+------------------------+",
-                player.name, player.hp, player.atk, player.armor,
-                "+------------------------+");
+                player.name, player.hp, player.atk, player.armor, "", player.eq[Weapon]->name, player.eq[Shield]->name, player.eq[Armor]->name,
+                "+------------------------+"); 
             Save(PlayerSaveFile, player);   
         }else{
-            printf("%s\n|%-24s|\n|Name\t: %-15s|\n|HP\t: %-15d|\n|ATK\t: %-15d|\n|DEF\t: %-15d|\n%s", 
-                "+------------------------+", "Saved Data",
-                player.name, player.hp, player.atk, player.armor,
+            printf("%s\n|Name\t: %-15s|\n|HP\t: %-15d|\n|ATK\t: %-15d|\n|DEF\t: %-15d|\n|EQ \t: %-15s|\n|\t- %-15s|\n|\t- %-15s|\n|\t- %-15s|\n%s", 
+                "+------------------------+",
+                player.name, player.hp, player.atk, player.armor, "", player.eq[Weapon]->name, player.eq[Shield]->name, player.eq[Armor]->name,
                 "+------------------------+");
         }
     }
@@ -51,18 +52,18 @@ int main () {
 
 void Start(Player *p){
     char charName[12];
-    Equipment startingEq = { "Stick", 1, 0,  Weapon };
+    Equipment startingEq1 = { "Stick", 1, 0, 0 };
+    Equipment startingEq2 = { "Shield", 0, 1, 1 };
+    Equipment startingEq3 = { "Armor", 0, 2, 2 };
     puts("Enter your character name (Max. 12 Character) : ");
     scanf("%[^\n]s", charName);
     strcpy(p->name, charName);
-    p->hp       = 5;
-    p->atk      = 2;
-    p->armor    = 0;
-    p->eq[0]    = &startingEq;
-}
-
-void getStats(){
-    
+    p->hp           = 5;
+    p->atk          = 2;
+    p->armor        = 0;
+    p->eq[Weapon]   = &startingEq1;
+    p->eq[Shield]   = &startingEq2;
+    p->eq[Armor]    = &startingEq3;
 }
 
 void Save(FILE *file, Player p){
