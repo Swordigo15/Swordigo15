e81d31b97f5a880f7fb1d8c18623c5b6a7a10199
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2022-12-16 10:44:15 +0700

    Update Muhammad Annys_L0122102_Responsi2.c

--- Konsep Pemrograman Pratikum/Muhammad Annys_L0122102_Responsi2.c
+++ Konsep Pemrograman Pratikum/Muhammad Annys_L0122102_Responsi2.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
 
-
+enum JenisKelamin { Pria, Wanita };
 
 typedef struct{
     char NIM[8];
     char nama[20];
+    int jk;
     float IPK;
-    enum JenisKelamin jk{ 
-        Pria, 
-        Wanita 
-    };
 }mahasiswa;
 
 int menu();
 
 int main()
 {
+    FILE* dataFile = NULL;
+    mahasiswa siswa[10];
+    
+    char fileName[15];
+    printf("Enter file Name : "); scanf("%s", fileName);
+    
+    if((dataFile = fopen(fileName, "rb+")) == NULL){
+        dataFile = fopen(fileName, "wb+");
+    }
+    
     switch(menu()){
         case 1:
         break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+        case 6:
+        break;
     }
+}
+
+void AddData(FILE *file){
+    int index;
+    char _NIM[8];
+    char _name[20];
+    printf("Enter NIM  : "); scanf("%s", _NIM);
+    printf("Enter name : "); scanf("%s", _name);
 }
 
 int menu(){
